73f591d0cb755d7df1b67a96d372da2fd3237572
Soren Stenback <firebloodmoderators@gmail.com>
2025-06-16 21:09:27 -0700

    Updated code to save results to csv
    See title.

--- LandCombatTest.py
+++ LandCombatTest.py
@@ -7,31 +7,45 @@ from LandCombatTestBattle import Battle
 ## Create list of levy numbers for testing use
 LevyNumbers = [100,200,300,400,500,600]
 
+## Create column labels for later use
+ColumnLabels = [f"{LevyNumber} CV" for LevyNumber in LevyNumbers]
+ColumnLabels.insert(0,"")
+
+## Set number of simulations
+SimulationCount = 100000
+
+## Initialise results list
+WinPercentageResults = []
+CasualtyResults = []
+
 ## Iterate through the LevyNumbers list to create 2 forces and simulate battle between them
 for LevyNumber1 in LevyNumbers:
+    ### Create Force 1
+    Force1 = Force(0,LevyNumber1)
+
+    ### Initialise new results lists
+    NewWinPercentageResults = [f"{LevyNumber1} CV"]
+    NewCasualtyPercentageResults = [f"{LevyNumber1} CV"]
+
     for LevyNumber2 in LevyNumbers:
-        ### Create Force 1
-        print(LevyNumber1)
-        Force1 = Force(0,LevyNumber1)
-        print(LevyNumber2)
-        ### Create Force 2
+        #### Create Force 2
         Force2 = Force(0,LevyNumber2)
 
-        ### Create variables to track wins and casualties
+        #### Create variables to track wins and casualties
         Force1Wins = 0
         Force1Casualties = 0
         Force2Wins = 0
         Force2Casualties = 0
 
-        ### Create battle between Force 1 and 2
+        #### Create battle between Force 1 and 2
         TestBattle = Battle(Force1,Force2)
 
-        ### Run sims 10,000 times
-        for x in range(10000):
-            ### Run battle between Force 1 and 2
+        #### Run sims 10,000 times
+        for x in range(SimulationCount):
+            ##### Run battle between Force 1 and 2
             BattleResult = TestBattle.battle()
 
-            ### Increment win count of winner of the battle
+            ##### Increment win count of winner of the battle
             if(BattleResult == 1):
                 Force1Wins = Force1Wins + 1
             elif(BattleResult == 2):
@@ -39,21 +53,31 @@ for LevyNumber1 in LevyNumbers:
             else:
                 pass
 
-            ### Increment casualty count of both sides
+            ##### Increment casualty count of both sides
             Force1Casualties = Force1Casualties + Force1.Casualties
             Force2Casualties = Force2Casualties + Force2.Casualties
 
-        ### Calculate win percentage and average casualties of each force
-        Force1WinPercentage = round((Force1Wins/10000)*100,2)
-        Force1AverageCasualties = round(Force1Casualties/10000)
-        Force2WinPercentage = round((Force2Wins/10000)*100,2)
-        Force2AverageCasualties = round(Force2Casualties/10000)
+        #### Calculate win percentage and average casualties of each force
+        Force1WinPercentage = round((Force1Wins/SimulationCount)*100,2)
+        Force1AverageCasualties = round(Force1Casualties/SimulationCount)
+        Force2WinPercentage = round((Force2Wins/SimulationCount)*100,2)
+        Force2AverageCasualties = round(Force2Casualties/SimulationCount)
 
-        ### Print results
-        print(f"""{LevyNumber1} CV vs. {LevyNumber2} CV
-              
-              {LevyNumber1} CV Win Percentage: {Force1WinPercentage}%
-              {LevyNumber1} CV Average Casualties: {Force1AverageCasualties}
-              
-              {LevyNumber2} CV Win Percentage: {Force2WinPercentage}%
-              {LevyNumber2} CV Average Casualties: {Force2AverageCasualties}""")
+        #### Save results to New Results lists
+        NewWinPercentageResults.append(f"{Force1WinPercentage}% Winrate")
+        NewCasualtyPercentageResults.append(f"{Force1AverageCasualties}% Casualties")
+    
+    ### Save New Results lists to Results list
+    WinPercentageResults.append(NewWinPercentageResults)
+    CasualtyResults.append(NewCasualtyPercentageResults)
+
+    ### Checkpoint
+    print(f"{LevyNumber1} simulations complete")
+
+## Save results to dataframe
+WinPercentageDataFrame = pd.DataFrame(WinPercentageResults,columns=ColumnLabels,)
+CasualtyDataFrame = pd.DataFrame(CasualtyResults,columns=ColumnLabels,)
+
+## Save dataframes to csv file
+WinPercentageDataFrame.to_csv("land_combat_small_battle_win_percentages.csv",index=False)
+CasualtyDataFrame.to_csv("land_combat_small_battle_casualties.csv",index=False)
