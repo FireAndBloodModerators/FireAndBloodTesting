3e81340859b2975de3f99fee5b6562713825264b
Soren Stenback <firebloodmoderators@gmail.com>
2025-06-23 12:17:13 -0700

    Added code to run simulations
    See title.

--- LandCombat/LargeBattles/LandCombatLargeBattlesTest.py
+++ LandCombat/LargeBattles/LandCombatLargeBattlesTest.py
@@ -1,11 +1,88 @@
 # IMPORTS
+import math
 import pandas as pd
-from LandCombatLargeBattlesTestFlank import Flank
 from LandCombatLargeBattlesTestForce import Force
 from LandCombatLargeBattlesTestBattle import Battle
 
 # TEST
-TestForce1 = Force(0,1500)
-TestForce2 = Force(0,3000)
-TestBattle = Battle(TestForce1,TestForce2)
-TestBattle.battle()
+## Create list of levy numbers for testing use
+LevyNumbers = [100,200,300,400,500,600,700]
+
+## Create column labels for later use
+ColumnLabels = [f"{math.floor(LevyNumber/3)} CV Left // {math.floor(LevyNumber/3) + (LevyNumber % 3)} CV Centre // {math.floor(LevyNumber/3)} CV Right" for LevyNumber in LevyNumbers]
+ColumnLabels.insert(0,"")
+
+## Set number of simulations
+SimulationCount = 100000
+
+## Initialise results list
+WinPercentageResults = []
+CasualtyResults = []
+
+## Iterate through the LevyNumbers list to create 2 forces and simulate battle between them
+for LevyNumber1 in LevyNumbers:
+    ### Create Force 1
+    Force1 = Force(0,LevyNumber1)
+
+    ### Initialise new results lists
+    NewWinPercentageResults = [f"{math.floor(LevyNumber1/3)} CV Left // {math.floor(LevyNumber1/3) + (LevyNumber1 % 3)} CV Centre // {math.floor(LevyNumber1/3)} CV Right"]
+    NewCasualtyPercentageResults = [f"{math.floor(LevyNumber1/3)} CV Left // {math.floor(LevyNumber1/3) + (LevyNumber1 % 3)} CV Centre // {math.floor(LevyNumber1/3)} CV Right"]
+
+    for LevyNumber2 in LevyNumbers:
+        #### Create Force 2
+        Force2 = Force(0,LevyNumber2)
+
+        #### Create variables to track wins and casualties
+        Force1Wins = 0
+        Force1LeftFlankCasualties = 0
+        Force1CentreFlankCasualties = 0
+        Force1RightFlankCasualties = 0
+        Force2Wins = 0
+        Force2LeftFlankCasualties = 0
+        Force2CentreFlankCasualties = 0
+        Force2RightFlankCasualties = 0
+
+        #### Create battle between Force 1 and 2
+        TestBattle = Battle(Force1,Force2)
+
+        #### Run sims X times
+        for x in range(SimulationCount):
+            ##### Run battle between Force 1 and 2
+            BattleResult = TestBattle.battle()
+
+            ##### Increment win count of winner of the battle
+            if(BattleResult == 1):
+                Force1Wins += 1
+            elif(BattleResult == 2):
+                Force2Wins += 1
+            else:
+                pass
+
+            ##### Increment casualty count of both sides
+            Force1LeftFlankCasualties += Force1.LeftFlank.Casualties
+            Force1CentreFlankCasualties += Force1.CentreFlank.Casualties
+            Force1RightFlankCasualties += Force1.RightFlank.Casualties
+            Force2LeftFlankCasualties += Force2.LeftFlank.Casualties
+            Force2CentreFlankCasualties += Force2.CentreFlank.Casualties
+            Force2RightFlankCasualties += Force2.RightFlank.Casualties
+
+        ##### Calculate win percentage and average casualties of each force
+        Force1WinPercentage = round((Force1Wins/SimulationCount)*100,2)
+        Force1LeftFlankAverageCasualties = round(Force1LeftFlankCasualties/SimulationCount)
+        Force1CentreFlankAverageCasualties = round(Force1CentreFlankCasualties/SimulationCount)
+        Force1RightFlankAverageCasualties = round(Force1RightFlankCasualties/SimulationCount)
+        Force2WinPercentage = round((Force2Wins/SimulationCount)*100,2)
+        Force2LeftFlankAverageCasualties = round(Force2LeftFlankCasualties/SimulationCount)
+        Force2CentreFlankAverageCasualties = round(Force2CentreFlankCasualties/SimulationCount)
+        Force2RightFlankAverageCasualties = round(Force2RightFlankCasualties/SimulationCount)
+
+        ##### Save results to New Results lists
+        NewWinPercentageResults.append(f"{Force1WinPercentage}% Winrate")
+        NewCasualtyPercentageResults.append(f"{Force1LeftFlankAverageCasualties}% Left Casualties // {Force1CentreFlankAverageCasualties}% Centre Casualties // {Force1RightFlankAverageCasualties}% Right Casualties")
+
+    ### Save New Results lists to Results list
+    WinPercentageResults.append(NewWinPercentageResults)
+    CasualtyResults.append(NewCasualtyPercentageResults)
+
+    #### Checkpoint
+    print(f"{math.floor(LevyNumber1/3)} CV Left // {math.floor(LevyNumber1/3) + (LevyNumber1 % 3)} CV Centre // {math.floor(LevyNumber1/3)} CV Right simulations complete")
