d6a5fbf74ea642c86948ff8fd1f80b8931e10ff0
Soren Stenback <firebloodmoderators@gmail.com>
2025-07-01 21:58:08 -0700

    Added formula to dynamically tally casualties.
    See title.

--- Sieges/SiegesTestSiege.py
+++ Sieges/SiegesTestSiege.py
@@ -26,8 +26,46 @@ class Siege:
         self.HoldfastSize:int = HoldfastSize
         self.OuterWallsDV:int = OuterWallsDV
         self.SiegeRollBonus:int = self.calculate_siege_roll_bonus()
+        self.BesiegersCasualties:int = 0
+        self.DefendersCasualties:int = 0
 
-    def calculate_siege_roll_bonus(self):
+    def calculate_siege_roll_bonus(self) -> int:
         """
         Function to calculate the starting Siege Roll Bonus based off of Holdfast Size and Outer Walls DV.
+
+        Returns:
+            int: The starting bonus to the Siege Rolls.
         """
+        if(self.OuterWallsDV > 0):
+            return math.floor(self.OuterWallsDV)
+        else:
+            return math.floor(self.HoldfastSize/2)
+        
+    def siege_roll(self):
+        """
+        Function to make a Siege Roll and add bonuses, calculating effect.
+
+        Returns:
+            int: Whether or not the Siege has ended. 1 for surrender, 2 for continue.
+        """
+        BaseSiegeRoll = random.randint(1,20)
+        SiegeRoll = BaseSiegeRoll + self.SiegeRollBonus
+        if(SiegeRoll <= 6):
+            if(BaseSiegeRoll == 1):
+                self.BesiegersCasualties += (10 * ((100-self.BesiegersCasualties)/100))
+            return 2
+        elif(SiegeRoll <= 15):
+            self.SiegeRollBonus += 2
+            self.DefendersCasualties += (2 * ((100-self.DefendersCasualties)/100))
+            return 2
+        elif(SiegeRoll <= 19):
+            self.SiegeRollBonus += 4
+            self.DefendersCasualties += (6 * ((100-self.DefendersCasualties)/100))
+            return 2
+        elif(SiegeRoll <= 22):
+            self.SiegeRollBonus += 8
+            self.DefendersCasualties += (10 * ((100-self.DefendersCasualties)/100))
+            return 2
+        elif(SiegeRoll <= 27):
+            self.SiegeRollBonus += 4
+            self.DefendersCasualties += (20 * ((100-self.DefendersCasualties)/100))
