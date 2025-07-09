cdf1df501fcec5858722a8e965c34bb415c08b82
Soren Stenback <firebloodmoderators@gmail.com>
2025-07-08 17:20:28 -0700

    Added Duel Roll and Damage Roll to Duellist class.
    See title.

--- Duels/DuelsTestDuellist.py
+++ Duels/DuelsTestDuellist.py
@@ -1,4 +1,5 @@
 # IMPORTS
+import random
 
 # CLASS
 class Duellist:
@@ -8,22 +9,53 @@ class Duellist:
     Attributes:
         Morale (int): The morale/fighting spirit of the Duellist.
         DuelBonus (int): The Duellist's bonus to Duel Rolls.
+        DamageBonus (int): The Duellist's bonus to Damage Rolls.
         MinorInjuries (int): The number of Minor Injuries the Duellist has taken.
         MajorInjuries (int): The number of Major Injuries the Duellist has taken.
         CriticalInjuries (int): The number of Critical Injuries the Duellist has taken.
         Deaths (int): The number of times the Duellist has died.
     """
 
-    def __init__(self,DuelBonus:int):
+    def __init__(self,DuelBonus:int,DamageBonus:int):
         """
         Initialiser function for a Duels Duellist.
     
         Arguments:
             DuelBonus (int): The Duellist's bonus to Duel Rolls.
+            DamageBonus (int): The Duellist's bonus to Damage Rolls.
         """
         self.Morale:int = 30
         self.DuelBonus:int = DuelBonus
+        self.DamageBonus:int = DamageBonus
         self.MinorInjuries:int = 0
         self.MajorInjuries:int = 0
         self.CriticalInjuries:int = 0
         self.Deaths:int = 0
+
+    def duel_roll(self) -> tuple[int,int|None]:
+        """
+        Function to roll 1d20 plus Duellist's Duel Bonus and return result and whether it was a Critical Strike.
+    
+        Returns:
+            DuelRoll (int): The Duellist's Duel Roll.
+            CriticalStrike (int|None): The Duellist's bonus to Damage Rolls.
+        """
+        DuelRoll = random.randint(1,20)
+        if(DuelRoll == 20):
+            CriticalStrike = 20
+        elif(DuelRoll == 1):
+            CriticalStrike = 1
+        else:
+            CriticalStrike = None
+        DuelRoll += self.DuelBonus
+        return DuelRoll,CriticalStrike
+    
+    def damage_roll(self) -> int:
+        """
+        Function to roll 2d5 plus Duellist's Damage Bonus and return result.
+
+        Returns:
+            DamageRoll (int): The Duellist's Damage Roll.
+        """
+        DamageRoll = random.randint(1,5) + random.randint(1,5) + self.DamageBonus
+        return DamageRoll
