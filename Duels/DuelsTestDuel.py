6cd7079aecd6b512711c16133433e5d9995e0705
Soren Stenback <firebloodmoderators@gmail.com>
2025-07-12 16:49:26 -0700

    Added Duel function to Duel class
    See title.

--- Duels/DuelsTestDuel.py
+++ Duels/DuelsTestDuel.py
@@ -21,3 +21,67 @@ class Duel:
         """
         self.Duellist1 = Duellist1
         self.Duellist2 = Duellist2
+
+    def reset_duellists(self):
+        """
+        Function to reset Duellist statistics to original values.
+        """
+        self.Duellist1.Morale = 30
+        self.Duellist1.DuelBonus = 0
+        self.Duellist1.MinorInjuries = 0
+        self.Duellist1.MajorInjuries = 0
+        self.Duellist1.CriticalInjuries = 0
+        self.Duellist1.Deaths = 0
+        self.Duellist2.Morale = 30
+        self.Duellist2.DuelBonus = 0
+        self.Duellist2.MinorInjuries = 0
+        self.Duellist2.MajorInjuries = 0
+        self.Duellist2.CriticalInjuries = 0
+        self.Duellist2.Deaths = 0
+
+    def duel(self) -> int:
+        """
+        Function to roll a Duel between two Duellists.
+
+        Returns:
+            DuelResult (int): The winner of the Duel. 1 for Duellist 1, 2 for Duellist 2, 0 for error.
+        """
+        self.reset_duellists()
+        while((not self.Duellist1.Defeated) & (not self.Duellist2.Defeated)):
+            Duellist1Roll,Duellist1CriticalStrike = self.Duellist1.duel_roll()
+            Duellist2Roll,Duellist2CriticalStrike = self.Duellist2.duel_roll()
+            if(Duellist1Roll > Duellist2Roll):
+                if((Duellist1CriticalStrike == 20) & (Duellist2CriticalStrike == 1)):
+                    self.Duellist2.Defeated = True
+                    break
+                elif((Duellist1CriticalStrike == 20) | (Duellist2CriticalStrike == 1)):
+                    self.Duellist2.injury_roll_a()
+                    self.Duellist2.Morale -= self.Duellist1.damage_roll()
+                else:
+                    self.Duellist2.Morale -= self.Duellist1.damage_roll()
+            elif(Duellist2Roll > Duellist1Roll):
+                if((Duellist2CriticalStrike == 20) & (Duellist1CriticalStrike == 1)):
+                    self.Duellist1.Defeated = True
+                    break
+                elif((Duellist2CriticalStrike == 20) | (Duellist1CriticalStrike == 1)):
+                    self.Duellist1.injury_roll_a()
+                    self.Duellist1.Morale -= self.Duellist2.damage_roll()
+                else:
+                    self.Duellist1.Morale -= self.Duellist2.damage_roll()
+            else:
+                pass
+            if(self.Duellist1.Morale <= 0):
+                self.Duellist1.Defeated = True
+            elif(self.Duellist2.Morale <= 0):
+                self.Duellist2.Defeated = True
+            else:
+                pass
+        if(self.Duellist1.Defeated & (not self.Duellist2.Defeated)):
+            DuelResult = 2
+            return DuelResult
+        elif(self.Duellist2.Defeated & (not self.Duellist1.Defeated)):
+            DuelResult = 1
+            return DuelResult
+        else:
+            print("Error in determining Duel victor.")
+            return 0
