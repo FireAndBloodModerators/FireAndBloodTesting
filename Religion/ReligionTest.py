105de31fccd0e9eff5c57c50b56d3db93a3a9ff3
Soren Stenback <firebloodmoderators@gmail.com>
2025-07-31 15:46:33 -0700

    Added code to test chance of failure
    See title.

--- Religion/ReligionTest.py
+++ Religion/ReligionTest.py
@@ -16,76 +16,85 @@ OwnProvinceList = [True,False]
 
 # CREATE COLUMN LABELS
 DurationColumnLabels = ["Conversion Type","Duration"]
+FailureColumnLabels = ["Conversion Type","Chance of Failure"]
 InjuryColumnLabels = ["Conversion Type","Average Deaths","Average Critical Injuries","Average Major Injuries","AverageMinorInjuries"]
 
 # INITIALISE RESULTS LISTS
 ConversionDurationResults = []
+ConversionFailureResults = []
 ConversionInjuryResults = []
 
 # ITERATE THROUGH EACH LIST TO CREATE CONVERSION OBJECT AND RUN SIMS
 for NewReligion in ReligionList:
     for CurrentReligion in ReligionList:
-        for Population in PopulationList:
-            for Control in ControlList:
-                for OwnProvince in OwnProvinceList:
-                    ## Initialise new results lists
-                    NewDurationResults = [f"New Religion: {NewReligion}, Current Religion: {CurrentReligion}, Population: {Population}, Control: {Control}, Own Province: {OwnProvince}"]
-                    NewInjuryResults = [f"New Religion: {NewReligion}, Current Religion: {CurrentReligion}, Population: {Population}, Control: {Control}, Own Province: {OwnProvince}"]
+        if(NewReligion != CurrentReligion):
+            for Population in PopulationList:
+                for Control in ControlList:
+                    for OwnProvince in OwnProvinceList:
+                        ## Initialise new results lists
+                        NewDurationResults = [f"New Religion: {NewReligion}, Current Religion: {CurrentReligion}, Population: {Population}, Control: {Control}, Own Province: {OwnProvince}"]
+                        NewFailureResults = [f"New Religion: {NewReligion}, Current Religion: {CurrentReligion}, Population: {Population}, Control: {Control}, Own Province: {OwnProvince}"]
+                        NewInjuryResults = [f"New Religion: {NewReligion}, Current Religion: {CurrentReligion}, Population: {Population}, Control: {Control}, Own Province: {OwnProvince}"]
 
-                    ## Create variables to track statistics
-                    NonFailedSimulations = SimulationCount
-                    DurationsSum = 0
-                    DeathsSum = 0
-                    CriticalInjuresSum = 0
-                    MajorInjuriesSum = 0
-                    MinorInjuriesSum = 0
+                        ## Create variables to track statistics
+                        DurationsSum = 0
+                        FailedSimulationsSum = 0
+                        DeathsSum = 0
+                        CriticalInjuresSum = 0
+                        MajorInjuriesSum = 0
+                        MinorInjuriesSum = 0
 
-                    ## Create Conversion object
-                    TestConversion = Conversion(NewReligion,CurrentReligion,Population,Control,OwnProvince)
+                        ## Create Conversion object
+                        TestConversion = Conversion(NewReligion,CurrentReligion,Population,Control,OwnProvince)
 
-                    ## Run simulations
-                    for x in range(SimulationCount):
-                        ### Run conversion
-                        NewDuration,ConversionFailed = TestConversion.conversion()
+                        ## Run simulations
+                        for x in range(SimulationCount):
+                            ### Run conversion
+                            NewDuration,ConversionFailed = TestConversion.conversion()
 
-                        ### Save statistics
-                        if(not ConversionFailed):
-                            DurationsSum += NewDuration
+                            ### Save statistics
+                            if(not ConversionFailed):
+                                DurationsSum += NewDuration
+                            else:
+                                FailedSimulationsSum += 1
+                            DeathsSum += TestConversion.Deaths
+                            CriticalInjuresSum += TestConversion.CriticalInjuries
+                            MajorInjuriesSum += TestConversion.MajorInjuries
+                            MinorInjuriesSum += TestConversion.MinorInjuries
+
+                        ## Calculate statistics
+                        if((SimulationCount - FailedSimulationsSum) > 0):
+                            NewAverageDuration = round((DurationsSum/(SimulationCount - FailedSimulationsSum)),2)
                         else:
-                            NonFailedSimulations -= 1
-                        DeathsSum += TestConversion.Deaths
-                        CriticalInjuresSum += TestConversion.CriticalInjuries
-                        MajorInjuriesSum += TestConversion.MajorInjuries
-                        MinorInjuriesSum += TestConversion.MinorInjuries
+                            NewAverageDuration = "N/A"
+                        NewChanceOfFailure = round((FailedSimulationsSum/SimulationCount*100),2)
+                        NewAverageDeaths = round((DeathsSum/SimulationCount),2)
+                        NewAverageCriticalInjuries = round((CriticalInjuresSum/SimulationCount),2)
+                        NewAverageMajorInjuries = round((MajorInjuriesSum/SimulationCount),2)
+                        NewAverageMinorInjuries = round((MinorInjuriesSum/SimulationCount),2)
 
-                    ## Calculate statistics
-                    if(NonFailedSimulations > 0):
-                        NewAverageDuration = round((DurationsSum/NonFailedSimulations),2)
-                    else:
-                        NewAverageDuration = "N/A"
-                    NewAverageDeaths = round((DeathsSum/SimulationCount),2)
-                    NewAverageCriticalInjuries = round((CriticalInjuresSum/SimulationCount),2)
-                    NewAverageMajorInjuries = round((MajorInjuriesSum/SimulationCount),2)
-                    NewAverageMinorInjuries = round((MinorInjuriesSum/SimulationCount),2)
+                        ## Save statistics to new results lists
+                        NewDurationResults.append(NewAverageDuration)
+                        NewFailureResults.append(f"{NewChanceOfFailure}%")
+                        NewInjuryResults.extend([NewAverageDeaths,NewAverageCriticalInjuries,NewAverageMajorInjuries,NewAverageMinorInjuries])
 
-                    ## Save statistics to new results lists
-                    NewDurationResults.append(NewAverageDuration)
-                    NewInjuryResults.extend([NewAverageDeaths,NewAverageCriticalInjuries,NewAverageMajorInjuries,NewAverageMinorInjuries])
+                        ## Save new results lists to results lists
+                        ConversionDurationResults.append(NewDurationResults)
+                        ConversionFailureResults.append(NewFailureResults)
+                        ConversionInjuryResults.append(NewInjuryResults)
 
-                    ## Save new results lists to results lists
-                    ConversionDurationResults.append(NewDurationResults)
-                    ConversionInjuryResults.append(NewInjuryResults)
-
-                    ## Checkpoint
-                    print(f"New Religion: {NewReligion}, Current Religion: {CurrentReligion}, Population: {Population}, Control: {Control}, Own Province: {OwnProvince} simulations complete")
+                        ## Checkpoint
+                        print(f"New Religion: {NewReligion}, Current Religion: {CurrentReligion}, Population: {Population}, Control: {Control}, Own Province: {OwnProvince} simulations complete")
 
 # SAVE RESULTS TO DATAFRAME
 ConversionDurationDataFrame = pd.DataFrame(ConversionDurationResults,columns=DurationColumnLabels)
+ConversionFailureDataFrame = pd.DataFrame(ConversionFailureResults,columns=FailureColumnLabels)
 ConversionInjuryDataFrame = pd.DataFrame(ConversionInjuryResults,columns=InjuryColumnLabels)
 
 ## Save simple dataframes to csv file
 ConversionDurationDataFrame.to_csv("Religion/religion_average_durations.csv",index=False)
+ConversionFailureDataFrame.to_csv("Religion/religion_chance_of_failure.csv",index=False)
 ConversionInjuryDataFrame.to_csv("Religion/religion_average_injuries.csv",index=False)
 
 # END TEST
-print("Conversion Sims ENd")
+print("Conversion Sims End")
