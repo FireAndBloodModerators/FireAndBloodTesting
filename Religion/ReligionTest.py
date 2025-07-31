b3d695983edf1842b505cec0c9efcdcc79ebbc5b
Soren Stenback <firebloodmoderators@gmail.com>
2025-07-31 15:04:31 -0700

    Added all required code for Religion testing
    See title.

--- Religion/ReligionTest.py
+++ Religion/ReligionTest.py
@@ -1,5 +1,91 @@
 # IMPORTS
 import pandas as pd
+from ReligionTestConversion import Conversion
 
-## SET SIMULATIONS COUNT
+# SET SIMULATIONS COUNT
 SimulationCount = 10000
+
+# BEGIN TEST
+print("Conversion Simulations Start")
+
+# CREATE VARIABLE LISTS
+ReligionList = ["Faith of the Seven","Old Gods","Skagosi Old Gods","DrownedGod","DrownedGodII","Mother Rhoyne","Gods of Valyria","R'hllor"]
+PopulationList = [1000,25000,50000,75000,100000,125000,150000,175000,200000,225000,250000,275000,300000]
+ControlList = [75,80,85,90,95,100]
+OwnProvinceList = [True,False]
+
+# CREATE COLUMN LABELS
+DurationColumnLabels = ["Conversion Type","Duration"]
+InjuryColumnLabels = ["Conversion Type","Average Deaths","Average Critical Injuries","Average Major Injuries","AverageMinorInjuries"]
+
+# INITIALISE RESULTS LISTS
+ConversionDurationResults = []
+ConversionInjuryResults = []
+
+# ITERATE THROUGH EACH LIST TO CREATE CONVERSION OBJECT AND RUN SIMS
+for NewReligion in ReligionList:
+    for CurrentReligion in ReligionList:
+        for Population in PopulationList:
+            for Control in ControlList:
+                for OwnProvince in OwnProvinceList:
+                    ## Initialise new results lists
+                    NewDurationResults = [f"New Religion: {NewReligion}, Current Religion: {CurrentReligion}, Population: {Population}, Control: {Control}, Own Province: {OwnProvince}"]
+                    NewInjuryResults = [f"New Religion: {NewReligion}, Current Religion: {CurrentReligion}, Population: {Population}, Control: {Control}, Own Province: {OwnProvince}"]
+
+                    ## Create variables to track statistics
+                    NonFailedSimulations = SimulationCount
+                    DurationsSum = 0
+                    DeathsSum = 0
+                    CriticalInjuresSum = 0
+                    MajorInjuriesSum = 0
+                    MinorInjuriesSum = 0
+
+                    ## Create Conversion object
+                    TestConversion = Conversion(NewReligion,CurrentReligion,Population,Control,OwnProvince)
+
+                    ## Run simulations
+                    for x in range(SimulationCount):
+                        ### Run conversion
+                        NewDuration,ConversionFailed = TestConversion.conversion()
+
+                        ### Save statistics
+                        if(not ConversionFailed):
+                            DurationsSum += NewDuration
+                        else:
+                            NonFailedSimulations -= 1
+                        DeathsSum += TestConversion.Deaths
+                        CriticalInjuresSum += TestConversion.CriticalInjuries
+                        MajorInjuriesSum += TestConversion.MajorInjuries
+                        MinorInjuriesSum += TestConversion.MinorInjuries
+
+                    ## Calculate statistics
+                    if(NonFailedSimulations > 0):
+                        NewAverageDuration = round((DurationsSum/NonFailedSimulations),2)
+                    else:
+                        NewAverageDuration = "N/A"
+                    NewAverageDeaths = round((DeathsSum/SimulationCount),2)
+                    NewAverageCriticalInjuries = round((CriticalInjuresSum/SimulationCount),2)
+                    NewAverageMajorInjuries = round((MajorInjuriesSum/SimulationCount),2)
+                    NewAverageMinorInjuries = round((MinorInjuriesSum/SimulationCount),2)
+
+                    ## Save statistics to new results lists
+                    NewDurationResults.append(NewAverageDuration)
+                    NewInjuryResults.extend([NewAverageDeaths,NewAverageCriticalInjuries,NewAverageMajorInjuries,NewAverageMinorInjuries])
+
+                    ## Save new results lists to results lists
+                    ConversionDurationResults.append(NewDurationResults)
+                    ConversionInjuryResults.append(NewInjuryResults)
+
+                    ## Checkpoint
+                    print(f"New Religion: {NewReligion}, Current Religion: {CurrentReligion}, Population: {Population}, Control: {Control}, Own Province: {OwnProvince} simulations complete")
+
+# SAVE RESULTS TO DATAFRAME
+ConversionDurationDataFrame = pd.DataFrame(ConversionDurationResults,columns=DurationColumnLabels)
+ConversionInjuryDataFrame = pd.DataFrame(ConversionInjuryResults,columns=InjuryColumnLabels)
+
+## Save simple dataframes to csv file
+ConversionDurationDataFrame.to_csv("Religion/religion_average_durations.csv",index=False)
+ConversionInjuryDataFrame.to_csv("Religion/religion_average_injuries.csv",index=False)
+
+# END TEST
+print("Conversion Sims ENd")
--- Religion/ReligionTestConversion.py
+++ Religion/ReligionTestConversion.py
@@ -1,4 +1,5 @@
 # IMPORTS
+import math
 import random
 
 # CLASS
@@ -7,29 +8,203 @@ class Conversion:
     A class representing a Religion Conversion for r/FireAndBlood.
 
     Attributes:
-        Morale (int): The morale/fighting spirit of the Duellist.
-        DuelBonus (int): The Duellist's bonus to Duel Rolls.
-        WeaponType (str): The weapon the Duellist is wielding.
-        DamageBonus (int): The Duellist's bonus to Damage Rolls.
-        MinorInjuries (int): The number of Minor Injuries the Duellist has taken.
-        MajorInjuries (int): The number of Major Injuries the Duellist has taken.
-        CriticalInjuries (int): The number of Critical Injuries the Duellist has taken.
-        Deaths (int): The number of times the Duellist has died.
-        Defeated (bool): Whether or not the Duellist is defeated.
+        NewReligion (str): The Religion the Province is being Converted to.
+        CurrentReligion (str): The current Religion of the Province.
+        Population (int): The total Population of the Province.
+        Control (int): The total Control of the Province.
+        OwnProvince (bool): Whether or not the Province is owned by the claim Converting it.
+        ConversionRollModifier (int): The modifier to the Conversion Roll.
+        Deaths (int): The number of deaths characters attempting to Convert the Province has suffered.
+        CriticalInjuries (int): The number of Critical Injuries characters attempting to Convert the Province have suffered.
+        Major Injuries (int): The number of Major Injuries characters attempting to Convert the Province have suffered.
+        MinorInjuries (int): The number of Minor Injuries characters attempting to Convert the Province have suffered.
     """
 
-    def __init__(self):
+    def __init__(self,NewReligion:str,CurrentReligion:str,Population:int,Control:int,OwnProvince:bool):
         """
         Initialiser function for a Religion Conversion.
 
         Arguments:
-            WeaponType (str): What weapon the Duellist is wielding, determines DamageBonus.
+            NewReligion (str): The Religion the Province is being Converted to.
+            CurrentReligion (str): The current Religion of the Province.
+            Population (int): The total Population of the Province.
+            Control (int): The total Control of the Province.
+            OwnProvince (bool): Whether or not the Province is owned by the claim Converting it.
         """
-        self.Morale:int = 30
-        self.DuelBonus:int = 0
-        self.DamageBonus:int = self.determine_damage_bonus()
-        self.MinorInjuries:int = 0
-        self.MajorInjuries:int = 0
-        self.CriticalInjuries:int = 0
+        self.NewReligion:str = NewReligion
+        self.CurrentReligion:str = CurrentReligion
+        self.Population:int = Population
+        self.Control:int = Control
+        self.OwnProvince:bool = OwnProvince
+        self.ConversionRollModifer:int = self.determine_starting_conversion_modifier()
         self.Deaths:int = 0
-        self.Defeated:bool = False
+        self.CriticalInjuries:int = 0
+        self.MajorInjuries:int = 0
+        self.MinorInjuries:int = 0
+
+    def determine_new_religion_conversion_modifier(self) -> int:
+        """
+        Function to determine Conversion Roll modifier for the new Religion.
+
+        Returns:
+            int: The modifier to the Conversion Roll from the new Religion.
+        """
+        if(self.NewReligion == "Skagosi Old Gods"):
+            return -8
+        elif(self.NewReligion == "DrownedGod"):
+            return -4
+        elif(self.NewReligion == "DrownedGodInII"):
+            return 2
+        elif(self.NewReligion == "MotherRhoyne"):
+            return -4
+        elif(self.NewReligion == "R'hllor"):
+            return -2
+        else:
+            return 0
+        
+    def determine_current_religion_conversion_modifier(self) -> int:
+        """
+        Function to determine Conversion Roll modifier for the current Religion.
+
+        Returns:
+            int: The modifier to the Conversion Roll from the current Religion.
+        """
+        if(self.CurrentReligion == "Old Gods"):
+            return 2
+        elif(self.CurrentReligion == "DrownedGod"):
+            return -2
+        elif(self.CurrentReligion == "MotherRhoyne"):
+            return -2
+        else:
+            return 0
+        
+    def determine_population_conversion_modifier(self) -> int:
+        """
+        Function to determine Conversion Roll modifier based on total Population.
+
+        Returns:
+            int: The modifier to the Conversion Roll from the total Population.
+        """
+        return -(math.floor(self.Population/25000))
+    
+    def determine_control_conversion_modifier(self) -> int:
+        """
+        Function to determine Conversion Roll modifier based on Control.
+
+        Returns:
+            int: The modifier to the Conversion Roll from the Control.
+        """
+        if(self.OwnProvince):
+            if(self.Control > 75):
+                return (math.floor((self.Control-75)/5))
+            else:
+                return 0
+        else:
+            if(self.Control > 75):
+                return -(math.floor((self.Control-75)/5))
+            else:
+                return 0
+    
+    def determine_starting_conversion_modifier(self) -> int:
+        """
+        Function to determine starting Conversion Roll modifier.
+
+        Returns:
+            ConversionRollModifier (int): The starting modifier to the Conversion Roll.
+        """
+        ConversionRollModifier = 0
+        ConversionRollModifier += self.determine_new_religion_conversion_modifier()
+        ConversionRollModifier += self.determine_current_religion_conversion_modifier()
+        ConversionRollModifier += self.determine_population_conversion_modifier()
+        ConversionRollModifier += self.determine_control_conversion_modifier()
+        if(ConversionRollModifier < -8):
+            ConversionRollModifier = -8
+        return ConversionRollModifier
+    
+    def injury_roll(self):
+        """
+        Function to make an Injury Roll.
+        """
+        InjuryRoll = random.randint(1,20)
+        if(InjuryRoll <= 2):
+            self.Deaths += 1
+        elif(InjuryRoll <= 6):
+            self.CriticalInjuries += 1
+        elif(InjuryRoll <= 12):
+            self.MajorInjuries += 1
+        else:
+            self.MinorInjuries += 1
+    
+    def conversion_roll(self) -> tuple[bool,bool]:
+        """
+        Function to make a Conversion Roll plus modifiers.
+
+        Returns:
+            ConversionOver (bool): Whether the Conversion had ended.
+            ConversionFailed (bool): Whether the Conversion ended in failure.
+        """
+        ConversionRoll = random.randint(1,20) + self.ConversionRollModifer
+        if(ConversionRoll <= 5):
+            ConversionOver = True
+            ConversionFailed = True
+            self.injury_roll()
+            return ConversionOver,ConversionFailed
+        elif(ConversionRoll <= 10):
+            ConversionOver = False
+            ConversionFailed = False
+            return ConversionOver,ConversionFailed
+        elif(ConversionRoll <= 15):
+            self.ConversionRollModifer += 1
+            ConversionOver = False
+            ConversionFailed = False
+            return ConversionOver,ConversionFailed
+        elif(ConversionRoll <= 22):
+            self.ConversionRollModifer += 2
+            ConversionOver = False
+            ConversionFailed = False
+            return ConversionOver,ConversionFailed
+        elif(ConversionRoll <= 30):
+            self.ConversionRollModifer += 4
+            ConversionOver = False
+            ConversionFailed = False
+            return ConversionOver,ConversionFailed
+        elif(ConversionRoll <= 36):
+            self.ConversionRollModifer += 6
+            ConversionOver = False
+            ConversionFailed = False
+            return ConversionOver,ConversionFailed
+        elif(ConversionRoll <= 39):
+            self.ConversionRollModifer += 8
+            ConversionOver = False
+            ConversionFailed = False
+            return ConversionOver,ConversionFailed
+        else:
+            ConversionOver = True
+            ConversionFailed = False
+            return ConversionOver,ConversionFailed
+    
+    def reset_conversion(self):
+        """
+        Function to reset Conversion statistics to original values.
+        """
+        self.ConversionRollModifer = self.determine_starting_conversion_modifier()
+        self.Deaths = 0
+        self.CriticalInjuries = 0
+        self.MajorInjuries = 0
+        self.MinorInjuries = 0
+    
+    def conversion(self) -> tuple[int,bool]:
+        """
+        Function to roll a Conversion for a Province.
+
+        Returns:
+            Duration (int): How many years it took to Convert the Province's Religion.
+        """
+        self.reset_conversion()
+        Duration = 0
+        ConversionOver = False
+        ConversionFailed = False
+        while(not ConversionOver):
+            Duration += 1
+            ConversionOver,ConversionFailed = self.conversion_roll()
+        return Duration,ConversionFailed
